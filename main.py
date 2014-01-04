31c77c3fdad79076c557823bd2ffccc8e71ed0f2
louvervecken <lou.vervecken@onsemi.com>
2014-01-04 23:50:44 +0100

    Added dashboard for alarm config
    Button flip is working.

--- main.py
+++ main.py
@@ -10,30 +10,43 @@ import sys
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 app = Flask(__name__.split('.')[0])
 
-motion_enabled = False
+alarm_enabled = False
 
 @app.route('/')
 def hello():
     """ Return hello template at application root URL."""
     return render_template('hello.html')
 
-@app.route('/alarm-config/get-motion')
-def get_motion_config():
-    """ Return the current configuration the alarm needs to be put in. """
-    return "motion_enabled = {0}".format(motion_enabled)
+@app.route('/alarm-config/dashboard')
+def alarm_dashboard():
+    """ Show user dashboard for alarm configuration and state. """
+    return render_template('alarm-dashboard.html',
+                           armed=alarm_enabled)
 
-@app.route('/alarm-config/set-motion', methods=['POST'])
-def set_motion_config():
+@app.route('/alarm-config/get')
+def get_alarm_config():
+    """ Return the current configuration the alarm needs to be put in. """
+    return "alarm_enabled = {0}".format(alarm_enabled)
+
+@app.route('/alarm-config/set', methods=['GET', 'POST'])
+def set_alarm_config():
     """
-    Config to set motion on or off.
+    Config to set alarm on-or off.
 
     example on how to set on client side:
     import requests
-    r = requests.post('https://rasp-lou-server.appspot.com/alarm-config/set-motion',
+    r = requests.post('https://rasp-lou-server.appspot.com/alarm-config/set',
                       data={'enabled': 'True'})
     """
-    global motion_enabled
-    motion_enabled = request.form.get('enabled') == 'True'
-    return "success", 201
+    global alarm_enabled
+    # if it is a post from the python client
+    if request.method=='POST':
+        alarm_enabled = request.form.get('enabled') == 'True'
+        return "success", 201
+    # or coming from dashboard
+    else:
+        alarm_enabled = request.args.get('enabled') == 'True'
+        return redirect('/alarm-config/dashboard')

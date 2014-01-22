537a5ffc455e0317cb65132bd6aa0ea05b23ec49
Lou Vervecken <lou.vervecken@onsemi.com>
2014-01-22 21:23:20 +0100

    heating switch added

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app = Flask(__name__.split('.')[0])
 
 class StateAndSettings(db.Model):
     alarm_enabled = db.BooleanProperty(default=False, indexed=False)
+    heating_enabled = db.BooleanProperty(default=False, indexed=False)
     cpu_temp = db.FloatProperty(default=0.0, indexed=False)
     ram_perc = db.FloatProperty(default=0.0, indexed=False)
     free_storage = db.FloatProperty(default=0.0, indexed=False)
@@ -29,7 +30,7 @@ class TemperatureMeasurement(db.Model):
     place = db.StringProperty(required=True)
     temperature = db.FloatProperty(required=True)
 
-state_version = 2
+state_version = 3
 state_name = 'state_{}'.format(state_version)
 
 state_key = db.Key.from_path('StateAndSettings', state_name)
@@ -50,7 +51,8 @@ def dashboard():
     """ Show user dashboard. """
     state = db.get(state_key)
     return render_template('dashboard.html',
-                           armed=state.alarm_enabled,
+                           alarm_enabled=state.alarm_enabled,
+                           heating_enabled=state.heating_enabled,
                            cpu_temp=state.cpu_temp,
                            ram_perc=state.ram_perc,
                            free_storage=state.free_storage,
@@ -83,6 +85,34 @@ def set_alarm_config():
     # or coming from dashboard
     else:
         state.alarm_enabled = request.args.get('enabled') == 'True'
+        state.put()
+        return redirect('/dashboard')
+
+@app.route('/heating-config/get')
+def get_heating_config():
+    """ Return the current configuration the heating needs to be put in. """
+    state = db.get(state_key)
+    return "heating_enabled = {0}".format(state.heating_enabled)
+
+@app.route('/heating-config/set', methods=['GET', 'POST'])
+def set_heating_config():
+    """
+    Config to set heating on-or off.
+
+    example on how to set on client side:
+    import requests
+    r = requests.post('https://rasp-lou-server.appspot.com/heating-config/set',
+                      data={'enabled': 'True'})
+    """
+    state = db.get(state_key)
+    # if it is a post from the python client
+    if request.method=='POST':
+        state.heating_enabled = request.form.get('enabled') == 'True'
+        state.put()
+        return "success", 201
+    # or coming from dashboard
+    else:
+        state.heating_enabled = request.args.get('enabled') == 'True'
         state.put()
         return redirect('/dashboard')
         
